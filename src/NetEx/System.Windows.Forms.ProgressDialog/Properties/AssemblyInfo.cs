6db26c7b8c54759e1767c8892225ca0b74d519b5
Simon Peckmore <11754624+Peckmore@users.noreply.github.com>
2018-09-05 10:11:08 +0100

    Add 'WinForms' to AssemblyInfo/NuSpec description to better indicate target usage of the library. Add 'WinForms' and 'ProgressDialog' to NuSpec tags to aid discoverability.

--- src/NetEx/System.Windows.Forms.ProgressDialog/Properties/AssemblyInfo.cs
+++ src/NetEx/System.Windows.Forms.ProgressDialog/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("System.Windows.Forms.ProgressDialog")]
-[assembly: AssemblyDescription("A managed implementation of the standard Windows progress dialog.")]
+[assembly: AssemblyDescription("A managed implementation of the standard Windows progress dialog for WinForms.")]
