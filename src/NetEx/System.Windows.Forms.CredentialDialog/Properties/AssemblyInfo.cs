ee666731a9b1cad313dc2c5ca1c5d5d70d684f62
Simon Peckmore <simon.peckmore@mail.nidec.com>
2024-10-17 16:28:21 +0100

    Project refactor after splitting into own repo.

--- src/NetEx/System.Windows.Forms.CredentialDialog/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("System.Windows.Forms.CredentialDialog")]
-[assembly: AssemblyDescription("A managed implementation of the standard Windows credential dialog for WinForms.")]
