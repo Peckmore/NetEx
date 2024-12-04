6f75bd211581f116461f77c1c2453675a3a37ca9
Simon Peckmore <11754624+Peckmore@users.noreply.github.com>
2024-12-04 13:45:22 +0000

    Delete src directory

--- src/NetExDemo/NetExDemo/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NetEx Test Client")]
-[assembly: AssemblyDescription("A client for testing and demonstrating features within the NetEx project.")]
