1ec9e198b097633cea8cb768ebcdece7a16ab505
Simon Peckmore <11754624+Peckmore@users.noreply.github.com>
2025-04-18 07:30:48 +0100

    Refactor for publishing - Renamed to `NetEx.WinForms.ProgressDialog` - Refactored project to reflect new name - Updated README

--- src/NetEx.WinForms.ProgressDialogDemo/ProgressDialogForm.cs
+++ src/NetEx.WinForms.ProgressDialogDemo/ProgressDialogForm.cs
@@ -3,7 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Windows.Forms;
 
-namespace NetEx.ProgressDialogDemo
+namespace NetEx.WinForms.ProgressDialogDemo
 {
     public partial class ProgressDialogForm : Form
     {
