3e224966e68e62a871493c7fc8a4729f16be0bea
Simon Peckmore <11754624+Peckmore@users.noreply.github.com>
2025-04-20 08:16:22 +0100

    Re-added credential project

---
+++ src/Demo/NetEx.WinForms.ProgressDialogDemo/CredentialDialog/CredentialDialogForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows.Forms;
+
+namespace NetEx.CredentialDialogDemo
+{
+    public partial class CredentialDialogForm : Form
+    {
+        #region Construction
+
+        [SuppressMessage("ReSharper", "LocalizableElement")]
+        public CredentialDialogForm()
+        {
+            InitializeComponent();
+
+#if NET20
+            frameworkToolStripStatusLabel.Text = ".Net Framework 2.0";
+#elif NET8_0
+            frameworkToolStripStatusLabel.Text = ".Net 8.0";
+#endif
+        }
+
+        #endregion
+
+        #region Methods
+
+        [SuppressMessage("ReSharper", "LocalizableElement")]
+        private void showDialogButton_Click(object sender, EventArgs e)
+        {
+            if (credentialDialog.ShowDialog() == DialogResult.OK)
+            {
+#if DEBUG
+                MessageBox.Show("Domain:\t" + credentialDialog.Domain + "\nUser:\t" + credentialDialog.Username + "\nPassword:\t" + credentialDialog.PasswordString);
+#else
+                MessageBox.Show("Domain:\t" + credentialDialog.Domain + "\nUser:\t" + credentialDialog.Username + "\nPassword is a SecureString and cannot be displayed without converting to a String.");
+#endif
+            }
+
+            // Refresh the property grid
+            credentialDialogPropertyGrid.Refresh();
+        }
+
+        #endregion
+    }
+}
