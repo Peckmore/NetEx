c71002a3647d816b744cb0a396123bede74fb890
Simon Peckmore <11754624+Peckmore@users.noreply.github.com>
2025-05-06 17:39:57 +0100

    Further stream updates.

--- src/NetEx.IO.MultiStream/StreamInfo.cs
+++ src/NetEx.IO.MultiStream/StreamInfo.cs
@@ -1,9 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
-namespace System.IO
+namespace NetEx.IO
 {
     /// <summary>
-    /// Provides a wrapper for a <see cref="IO.Stream"/> that contains additional information about where the stream starts and ends as part
+    /// Provides a wrapper for a <see cref="System.IO.Stream"/> that contains additional information about where the stream starts and ends as part
     /// of a <see cref="MultiStream"/>.
     /// </summary>
     internal sealed class StreamInfo : IDisposable
@@ -19,16 +20,14 @@ namespace System.IO
         /// <summary>  
         /// Creates a new <see cref="StreamInfo"/> instance.  
         /// </summary>  
-        /// <param name="stream">The base <see cref="IO.Stream"/> to wrap.</param>  
-        /// <param name="start">The start position of the specified <see cref="IO.Stream"/> within the single, merged stream.</param>  
-        /// <param name="filename">The name of the file associated with the specified <see cref="IO.Stream"/>.</param>  
-        public StreamInfo(Stream stream, long start, string filename)
+        /// <param name="stream">The base <see cref="System.IO.Stream"/> to wrap.</param>  
+        /// <param name="start">The start position of the specified <see cref="System.IO.Stream"/> within the single, merged stream.</param>
+        public StreamInfo(Stream stream, long start)
         {
             Stream = stream;
             Start = start;
             Length = stream.Length;
             End = start + Length - 1;
-            Filename = filename;
         }
 
         #endregion
@@ -36,23 +35,34 @@ namespace System.IO
         #region Properties
 
         /// <summary>
-        /// The end position of the wrapped <see cref="IO.Stream"/> within the <see cref="MultiStream"/>.
+        /// The end position of the wrapped <see cref="System.IO.Stream"/> within the <see cref="MultiStream"/>.
         /// </summary>
         public long End { get; private set; }
         /// <summary>
-        /// The name of the file the wrapped <see cref="IO.Stream"/> is accessing.
-        /// </summary>
-        public string Filename { get; private set; }
-        /// <summary>
-        /// The length of the wrapped <see cref="IO.Stream"/>.
+        /// The length of the wrapped <see cref="System.IO.Stream"/>.
         /// </summary>
         public long Length { get; private set; }
         /// <summary>
-        /// The start position of the wrapped <see cref="IO.Stream"/> within the <see cref="MultiStream"/>.
+        /// The name of the file the wrapped <see cref="System.IO.Stream"/> is accessing.
+        /// </summary>
+        public string? Name
+        {
+            get
+            {
+                if (Stream is FileStream fileStream)
+                {
+                    return fileStream.Name;
+                }
+
+                return null;
+            }
+        }
+        /// <summary>
+        /// The start position of the wrapped <see cref="System.IO.Stream"/> within the <see cref="MultiStream"/>.
         /// </summary>
         public long Start { get; private set; }
         /// <summary>
-        /// The wrapped <see cref="IO.Stream"/>.
+        /// The wrapped <see cref="System.IO.Stream"/>.
         /// </summary>
         public Stream Stream { get; private set; }
 
@@ -75,8 +85,6 @@ namespace System.IO
 
                 _isDisposed = true;
             }
-
-            GC.SuppressFinalize(this);
         }
 
         #endregion
